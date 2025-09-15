5fe0447bac6cd6533f6ae874042ab3f719a97800
Chenjx12 <2446891342@qq.com>
2025-09-16 01:20:08 +0800

    init

--- app/setup.py
+++ app/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="pychat",
     version="0.1.0",
-    packages=find_packages(),   # 自动发现 app 包
+    packages=["app"],  # 自动发现 app 包
     python_requires=">=3.11",
 )
